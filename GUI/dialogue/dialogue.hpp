9f4d1b0b3c8203fe14d4f5cf5614e174c22f44e7
Asmo <asmo.games.341@gmail.com>
2025-01-09 20:55:53 +0400

    Rework dialogue UI for all resolutions

--- GUI/dialogue/dialogue.hpp
+++ GUI/dialogue/dialogue.hpp
@@ -1,21 +1,34 @@
+//auto includes by description.ext
+// #include "GUI\ui_toolkit.hpp"
+
 //moved to imports.hpp
 // import RscListbox;
 
-#define UI_GRID_X	(0.5)
-#define UI_GRID_Y	(0.5)
-#define UI_GRID_W	(2.5 * pixelW * pixelGrid)
-#define UI_GRID_H	(2.5 * pixelH * pixelGrid)
-#define UI_GRID_WAbs	(0)
-#define UI_GRID_HAbs	(0)
-
-//--- dialogueUI
+//--- dialogueUI IDCs
 #define IDC_QLISTBOX 1500
 #define IDC_ALISTBOX 1501
 #define IDC_TEXT_LEFT 1000
 #define IDC_TEXT_RIGHT 1001
 #define IDC_TEXT_NPC 1002
 
+//--- font
 #define FONT_NAME "EtelkaMonospacePro"
+
+//--- scale helpers
+#define LISTBOX_W  (0.875 * X_SCALE)
+#define QLISTBOX_H (0.5 * Y_SCALE)
+#define ALISTBOX_H (0.2 * Y_SCALE)
+#define TEXT_W (0.15 * X_SCALE)
+#define TEXT_H (0.03 * Y_SCALE)
+
+//--- position helpers
+#define LISTBOX_X CENTER(1,(LISTBOX_W))
+#define QLISTBOX_Y (CENTER(1,(0.70 * Y_SCALE)))
+#define ALISTBOX_Y (CENTER(1,(-0.40 * Y_SCALE)))
+#define TEXT_LEFT_X   (CENTER(1,(1.0 * LISTBOX_W)))
+#define TEXT_RIGHT_X  ((CENTER(1,(-1.0 * LISTBOX_W))) - TEXT_W)
+#define TEXT_TOP_Y    (QLISTBOX_Y - TEXT_H)
+#define TEXT_BOTTOM_Y (ALISTBOX_Y - TEXT_H)
 
 class dialogueUI {
     idd = 7103;
@@ -28,10 +41,10 @@ class dialogueUI {
         class DUI_QListbox: RscListbox
         {
             idc = IDC_QLISTBOX;
-            x = -20 * UI_GRID_W + UI_GRID_X;
-            y = -13 * UI_GRID_H + UI_GRID_Y;
-            w = 40 * UI_GRID_W;
-            h = 18 * UI_GRID_H;
+            x = LISTBOX_X;
+            y = QLISTBOX_Y;
+            w = LISTBOX_W;
+            h = QLISTBOX_H;
             shadow = 1;
             font = FONT_NAME;
             colorBackground[] = {0,0,0,0.5};
@@ -39,10 +52,10 @@ class dialogueUI {
         class DUI_AListbox: RscListbox
         {
             idc = IDC_ALISTBOX;
-            x = -20 * UI_GRID_W + UI_GRID_X;
-            y = 7 * UI_GRID_H + UI_GRID_Y;
-            w = 40 * UI_GRID_W;
-            h = 8 * UI_GRID_H;
+            x = LISTBOX_X;
+            y = ALISTBOX_Y;
+            w = LISTBOX_W;
+            h = ALISTBOX_H;
             shadow = 1;
             font = FONT_NAME;
             colorBackground[] = {0,0,0,0.55};
@@ -50,30 +63,30 @@ class dialogueUI {
         class DUI_TextLeft: RscText
         {
             idc = IDC_TEXT_LEFT;
-            x = -20 * UI_GRID_W + UI_GRID_X;
-            y = 6 * UI_GRID_H + UI_GRID_Y;
-            w = 8 * UI_GRID_W;
-            h = 1 * UI_GRID_H;
+            x = TEXT_LEFT_X;
+            y = TEXT_BOTTOM_Y;
+            w = TEXT_W;
+            h = TEXT_H;
             colorBackground[] = {0,0,0,0.55};
         };
         class DUI_TextRight: RscText
         {
             idc = IDC_TEXT_RIGHT;
             style = 1;//1: align right
-            x = 12 * UI_GRID_W + UI_GRID_X;
-            y = 6 * UI_GRID_H + UI_GRID_Y;
-            w = 8 * UI_GRID_W;
-            h = 1 * UI_GRID_H;
+            x = TEXT_RIGHT_X;
+            y = TEXT_BOTTOM_Y;
+            w = TEXT_W;
+            h = TEXT_H;
             colorBackground[] = {0,0,0,0.55};
         };
         class DUI_TextNPC: RscText
         {
             idc = IDC_TEXT_NPC;
             style = 1;//1: align right
-            x = 12 * UI_GRID_W + UI_GRID_X;
-            y = -14 * UI_GRID_H + UI_GRID_Y;
-            w = 8 * UI_GRID_W;
-            h = 1 * UI_GRID_H;
+            x = TEXT_RIGHT_X;
+            y = TEXT_TOP_Y;
+            w = TEXT_W;
+            h = TEXT_H;
             colorBackground[] = {0,0,0,0.5};
         };
         ////////////////////////////////////////////////////////
